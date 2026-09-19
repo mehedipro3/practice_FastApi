6e1f89b48893d81e62662641fb3ace9774f24f9d
mehedipro3 <mehediratul37@gmail.com>
2026-09-20 00:51:30 +0600

    sort by parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 
 app = FastAPI()
@@ -31,3 +31,26 @@ def view_students(student_id: str = Path(..., description="The ID of the student
         return data[student_id]
     else:
         raise HTTPException(status_code = 404, detail="Student not found")
+    
+    
+    
+@app.get("/sort")
+def sort_students(sorted_by: str = Query(..., description="The field to sort students by"), order : str = Query(description = "sort by asc or desc")):
+    
+    vaild_fields = ['roll', 'class', 'phn']
+    
+    
+    if sorted_by not in vaild_fields:
+        raise HTTPException(status_code = 404, detail=f"Invalid sort field {vaild_fields}")
+    
+    if order not in ['asc' , 'desc']:
+        raise HTTPException(status_code = 404, detail=f"Invalid order. Choose between asc and desc")
+    
+    data = load_data()
+    
+    sort_order = True if order == 'desc' else False
+    
+    sorted_data = list(data.values())
+    sorted_data.sort(key = lambda x: x[sorted_by] , reverse=sort_order)
+     
+    return sorted_data
