e79d6f47c5aef17c19fdcd8078f37f47cc9ddcf1
mehedipro3 <mehediratul37@gmail.com>
2026-09-25 00:13:00 +0600

    post will be added

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from fastapi import FastAPI,Path,HTTPException,Query
+from fastapi import FastAPI,Path,HTTPException,Query , Body
+from pydantic import BaseModel ,Field
+from typing import Annotated
+from fastapi.responses import JSONResponse
 import json
 
 app = FastAPI()
+
+class Student(BaseModel):
+    id: Annotated[str , Field(..., description="The ID of the student", example="S001")]
+    name: Annotated[str , Field(..., description="The name of the student", example="John Doe")]
+    roll: Annotated[int , Field(..., description="The roll number of the student", example=1)]
+    class_: Annotated[str , Field(..., description="The class of the student", example="10A")]
+    phn: Annotated[str , Field(..., description="The phone number of the student", example="1234567890")]
+    marks: Annotated[dict, Field(..., description="The marks of the student", example={
+      "mathematics": 89,
+      "physics": 91,
+      "chemistry": 86,
+      "english": 93,
+      "computer_science": 96
+    })]
 
 def load_data():
     with open('students.json','r') as f:
         data = json.load(f)
     return data
+
+
+def save_data(data):
+    with open('students.json','w') as f:
+        json.dump(data, f)
+        
+        
+
 
 @app.get("/")
 def hello():
@@ -23,7 +48,7 @@ def view_students():
 
 
 @app.get("/view/{student_id}")
-def view_students(student_id: str = Path(..., description="The ID of the student to retrieve" , example="S002")):
+def view_students(student_id: str = Path(..., description="The ID of the student to retrieve", examples=["S002"])):
     data = load_data()
     
     
@@ -35,7 +60,7 @@ def view_students(student_id: str = Path(..., description="The ID of the student
     
     
 @app.get("/sort")
-def sort_students(sorted_by: str = Query(..., description="The field to sort students by"), order : str = Query(description = "sort by asc or desc")):
+def sort_students(sorted_by: str = Query(..., description="The field to sort students by"), order: str = Query("asc", description="Sort by asc or desc")):
     
     vaild_fields = ['roll', 'class', 'phn']
     
@@ -54,3 +79,20 @@ def sort_students(sorted_by: str = Query(..., description="The field to sort stu
     sorted_data.sort(key = lambda x: x[sorted_by] , reverse=sort_order)
      
     return sorted_data
+
+
+
+@app.post('/create')
+def create_student(student: Student):
+    data = load_data()
+    
+    if student.id in data:
+        raise HTTPException(status_code = 400, detail="Student with this ID already exists")
+    
+    student_id = student.id
+    data[student_id] = student.model_dump(exclude='id')
+    marks = student.marks
+    data[student_id]['marks'] = marks
+    
+    save_data(data)
+    return JSONResponse(content={"message": "Student created successfully"}, status_code=201)
